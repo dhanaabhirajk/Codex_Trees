deaf3c2c4227bcfb8f49b14cb34cbaa7f89ae050
dhanaabhirajk <dhanaabhirajk@gmail.com>
2022-08-30 07:34:00 +0530

    google oauth2 user selection login bug rectify

--- src/main/java/com/codextrees/web/common/UnauthenticatedRequestHandler.java
+++ src/main/java/com/codextrees/web/common/UnauthenticatedRequestHandler.java
@@ -12,11 +12,12 @@ public class UnauthenticatedRequestHandler implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        if (request.getServletPath().startsWith("/admin/")) {
-            response.setStatus(403);
-        } else {
-        	//response.setStatus(403);
-            response.sendRedirect("/oauth2/authorization/google");
-        }
+//        if (request.getServletPath().startsWith("/admin/")) {
+//            response.setStatus(403);
+//        } else {
+//        	//response.setStatus(403);
+//            response.sendRedirect("/oauth2/authorization/google");
+//        }
+    	response.setStatus(403);
     }
 }
